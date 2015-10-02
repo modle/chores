4bfa0395dc0661890d087c96197be35cb0b2e120
modle <odle.matthew@gmail.com>
2015-10-01 20:46:35 -0500

    updated logic on due date and completed date visibility

--- chores/templatetags/chores_extras.py
+++ chores/templatetags/chores_extras.py
@@ -10,3 +10,8 @@ def overdue(value, frequency):
     if days > 90:
         days = 999
     return days
+
+@register.filter(name='absolute')
+def absolute(value):
+    absolute_value = abs(value)
+    return absolute_value
