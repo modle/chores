f4a75162add59954d484b0b6f4a4457e2c62b1fd
matt <odle.matthew@gmail.com>
2015-10-02 20:59:55 -0500

    added frequency display to profile and all chores

--- house/settings.py
+++ house/settings.py
@@ -3,25 +3,11 @@ Django settings for house.
 
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +57,6 @@ WSGI_APPLICATION = 'house.wsgi.application'
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -85,10 +70,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-
-
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/house/staticfiles/'
