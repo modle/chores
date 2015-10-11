c0fcf08c545b1ab3836d99fdfc46b4938cb5de07
modle <odle.matthew@gmail.com>
2015-10-10 22:11:28 -0500

    admin models; updated messages on all chores

--- chores/admin.py
+++ chores/admin.py
@@ -1,17 +1,24 @@
 from django.contrib import admin
-from chores.models import Chores, Category, History
+from chores.models import Chores, Category, History, Score
 
-class BlogAdmin(admin.ModelAdmin):
+class ChoreAdmin(admin.ModelAdmin):
     exclude = ['posted']
     prepopulated_fields = {'slug': ('title',)}
 
+
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+
 
 class HistoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {}
 
 
-admin.site.register(Chores, BlogAdmin)
+class ScoreAdmin(admin.ModelAdmin):
+    prepopulated_fields = {}
+
+
+admin.site.register(Chores, ChoreAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(History, HistoryAdmin)
+admin.site.register(Score, ScoreAdmin)
