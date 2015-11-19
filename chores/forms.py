dd2c1af1e124dc7e8d0a2e4b5f6447b920c62e9c
modle <odle.matthew@gmail.com>
2015-11-18 23:01:19 -0600

    more login form styles

--- chores/forms.py
+++ chores/forms.py
@@ -74,7 +74,11 @@ class MyAuthenticationForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(MyAuthenticationForm, self).__init__(*args, **kwargs)
         self.base_fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.base_fields['username'].widget.attrs['id'] = 'login__username'
+        self.base_fields['username'].widget.attrs['class'] = 'form__input'
         self.base_fields['password'].widget.attrs['placeholder'] = 'Password'
+        self.base_fields['password'].widget.attrs['id'] = 'login__password'
+        self.base_fields['password'].widget.attrs['class'] = 'form__input'
 
     class Meta:
         fields = ('username', 'password', )
